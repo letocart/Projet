cba671c6fd41181d2e293b42dc3deec0c5bdc1e3
camille24121998 <camille24121998@live.fr>
2020-12-09 00:56:38 +0100

    Fusion prototypes 1 et 2

--- flutter_prototype1/lib/main.dart
+++ flutter_prototype1/lib/main.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
-void main() => runApp(MyApp());
-
+void main() {
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
@@ -14,34 +15,74 @@ class MyApp extends StatelessWidget {
     ]);
     return MaterialApp(
       title: 'Jouons à la RO',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: Colors.teal,
+        scaffoldBackgroundColor: Colors.white70,
+        // This makes the visual density adapt to the platform that you run
+        // the app on. For desktop platforms, the controls will be smaller and
+        // closer together (more dense) than on mobile platforms.
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
       home: Scaffold(
         appBar: AppBar(
+          // Here we take the value from the MyHomePage object that was created by
+          // the App.build method, and use it to set our appbar title.
           title: Text('Jouons à la RO'),
         ),
         body: Center(
-          child: Row(
+          // Center is a layout widget. It takes a single child and positions it
+          // in the middle of the parent.
+          child: Column(
+            // Column is also a layout widget. It takes a list of children and
+            // arranges them vertically. By default, it sizes itself to fit its
+            // children horizontally, and tries to be as tall as its parent.
+            //
+            // Invoke "debug painting" (press "p" in the console, choose the
+            // "Toggle Debug Paint" action from the Flutter Inspector in Android
+            // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
+            // to see the wireframe for each widget.
+            //
+            // Column has various properties to control how it sizes itself and
+            // how it positions its children. Here we use mainAxisAlignment to
+            // center the children vertically; the main axis here is the vertical
+            // axis because Columns are vertical (the cross axis would be
+            // horizontal).
+            mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
-              Container(
-                child: Text('Je suis un personnage')
+              TextButton(
+                  onPressed: () {
+                    print('Jeu 1 pressed');
+                  },
+                  style : TextButton.styleFrom
+                    (
+                    primary: Colors.white,
+                    backgroundColor: Colors.teal,
+                    onSurface: Colors.grey,
+                  ),
+                  child: Text('Jeu n°1')
               ),
-              Container(
-                child: Column(
-                  children:<Widget>[
-                    TextButton(
-                      child: Text('Jeu n°1'),
-                      onPressed: (){
-                        print('Jeu 1 pressed');
-                      }
-                    ),
-                    TextButton(
-                        child: Text('Jeu n°2'),
-                        onPressed: (){
-                          print('Jeu 2 pressed');
-                        }
-                    )
-                  ]
-                )
+              TextButton(
+                  onPressed: (){
+                    print('Jeu 2 pressed');
+                  },
+                  style : TextButton.styleFrom
+                    (
+                    primary: Colors.white,
+                    backgroundColor: Colors.teal,
+                    onSurface: Colors.grey,
+                  ),
+                  child: Text('Jeu n°2')
               ),
+              Text('Je suis un personnage'),
             ],
           ),
         ),
