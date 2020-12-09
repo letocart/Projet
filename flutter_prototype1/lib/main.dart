071e88fe638aab71f0f85b066784df896ffab5f3
camille24121998 <camille24121998@live.fr>
2020-12-09 16:19:55 +0100

    Navigation entre le menu et les jeux et retours

--- flutter_prototype1/lib/main.dart
+++ flutter_prototype1/lib/main.dart
@@ -2,10 +2,15 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(
+      MaterialApp(
+        title: 'Notre application',
+        home: Menu(),
+        )
+  );
 }
 
-class MyApp extends StatelessWidget {
+class Menu extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
@@ -16,77 +21,151 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Jouons à la RO',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.teal,
         scaffoldBackgroundColor: Colors.white70,
-        // This makes the visual density adapt to the platform that you run
-        // the app on. For desktop platforms, the controls will be smaller and
-        // closer together (more dense) than on mobile platforms.
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
       home: Scaffold(
         appBar: AppBar(
-          // Here we take the value from the MyHomePage object that was created by
-          // the App.build method, and use it to set our appbar title.
           title: Text('Jouons à la RO'),
         ),
         body: Center(
-          // Center is a layout widget. It takes a single child and positions it
-          // in the middle of the parent.
-          child: Column(
-            // Column is also a layout widget. It takes a list of children and
-            // arranges them vertically. By default, it sizes itself to fit its
-            // children horizontally, and tries to be as tall as its parent.
-            //
-            // Invoke "debug painting" (press "p" in the console, choose the
-            // "Toggle Debug Paint" action from the Flutter Inspector in Android
-            // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-            // to see the wireframe for each widget.
-            //
-            // Column has various properties to control how it sizes itself and
-            // how it positions its children. Here we use mainAxisAlignment to
-            // center the children vertically; the main axis here is the vertical
-            // axis because Columns are vertical (the cross axis would be
-            // horizontal).
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              TextButton(
-                  onPressed: () {
-                    print('Jeu 1 pressed');
-                  },
-                  style : TextButton.styleFrom
-                    (
-                    primary: Colors.white,
-                    backgroundColor: Colors.teal,
-                    onSurface: Colors.grey,
-                  ),
-                  child: Text('Jeu n°1')
+          child : Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children : [
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children : [Text('Je suis un personnage')]
               ),
-              TextButton(
-                  onPressed: (){
-                    print('Jeu 2 pressed');
-                  },
-                  style : TextButton.styleFrom
-                    (
-                    primary: Colors.white,
-                    backgroundColor: Colors.teal,
-                    onSurface: Colors.grey,
-                  ),
-                  child: Text('Jeu n°2')
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children : [
+                TextButton(
+                    onPressed: () {
+                      print("Jeu n°2 pressed");
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(builder: (context) => Jeu1())
+                      );
+                    },
+                    style : TextButton.styleFrom
+                      (
+                      primary: Colors.white,
+                      backgroundColor: Colors.teal,
+                      onSurface: Colors.grey,
+                    ),
+                    child: Text('Jeu n°1')
+                ),
+                TextButton(
+                    onPressed: (){
+                      print('Jeu 2 pressed');
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(builder: (context) => Jeu2())
+                      );
+                    },
+                    style : TextButton.styleFrom
+                      (
+                      primary: Colors.white,
+                      backgroundColor: Colors.teal,
+                      onSurface: Colors.grey,
+                    ),
+                    child: Text('Jeu n°2')
+                ),
+                ],
               ),
-              Text('Je suis un personnage'),
             ],
           ),
         ),
       ),
+    );
+  }
+}
+
+class Jeu1 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.landscapeLeft,
+      DeviceOrientation.landscapeRight,
+    ]);
+    return MaterialApp(
+        title: "Jeu d'ordonnancement à n machine",
+        theme: ThemeData(
+          primarySwatch: Colors.teal,
+          scaffoldBackgroundColor: Colors.blueGrey,
+          visualDensity: VisualDensity.adaptivePlatformDensity,
+        ),
+        home: Scaffold(
+          appBar: AppBar(
+            title: Text("Jeu d'ordonnancement à n machine"),
+            ),
+          body: Center(
+            child : Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children : [
+                Text("Jeu n°1"),
+                TextButton(
+                    onPressed: (){
+                      print('Return pressed');
+                      Navigator.pop(context);
+                    },
+                    style : TextButton.styleFrom
+                      (
+                      primary: Colors.white,
+                      backgroundColor: Colors.teal,
+                      onSurface: Colors.grey,
+                    ),
+                    child: Text('Retour')
+                  ),
+                ],
+              )
+            )
+        )
+    );
+  }
+}
+
+class Jeu2 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.landscapeLeft,
+      DeviceOrientation.landscapeRight,
+    ]);
+    return MaterialApp(
+        title: "Jeu du voyageur de commerce",
+        theme: ThemeData(
+          primarySwatch: Colors.teal,
+          scaffoldBackgroundColor: Colors.amberAccent,
+          visualDensity: VisualDensity.adaptivePlatformDensity,
+        ),
+        home: Scaffold(
+            appBar: AppBar(
+              title: Text("Jeu du voyageur de commerce"),
+            ),
+            body: Center(
+                child : Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children : [
+                    Text("Jeu n°2"),
+                    TextButton(
+                        onPressed: (){
+                          print('Return pressed');
+                          Navigator.pop(context);
+                        },
+                        style : TextButton.styleFrom
+                          (
+                          primary: Colors.white,
+                          backgroundColor: Colors.teal,
+                          onSurface: Colors.grey,
+                        ),
+                        child: Text('Retour')
+                    ),
+                  ],
+                )
+            )
+        )
     );
   }
 }
