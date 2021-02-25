03fbdd8726ebc77dc93e04f89bcb4944a31c78af
camille24121998 <camille24121998@live.fr>
2021-02-25 12:31:11 +0100

    Redimentionnement du perseonnage -> Renomage du package non fait

--- flutter_prototype3/lib/widgets/ScreenMenuPrincipal.dart
+++ flutter_prototype3/lib/widgets/ScreenMenuPrincipal.dart
@@ -12,29 +12,43 @@ class ScreenMenuPrincipal extends StatelessWidget {
       child : Row(
         mainAxisAlignment: MainAxisAlignment.spaceAround,
         children : [
-          Image.asset('assets/images/professeur.png'),
-          Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children : [
-              RaisedButton(
-                  onPressed: () {
-                    print("jeu 1 pressed");
-                  },
-                  color : Colors.orange,
-                  shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.all(Radius.circular(30))),
-                  child: Text('Game 1',style: TextStyle(color:Colors.black),)
-              ),
-              RaisedButton(
-                  onPressed: () {
-                    print("Jeu 2 pressed");
-                  },
-                  color : Colors.orange,
-                  shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.all(Radius.circular(30))),
-                  child: Text('Game 2',style: TextStyle(color:Colors.black),)
-              ),
-            ],
+          Expanded(
+            flex : 1,
+            child : Container(
+                child : Align(
+                    alignment : Alignment.bottomLeft,
+                    child : Padding (
+                        padding: EdgeInsets.all(16.0),
+                        child : Image.asset('assets/images/professeur.png', scale : 10.0)
+                    )
+                )
+            )
+          ),
+          Expanded(
+              flex : 1,
+              child : Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children : [
+                  RaisedButton(
+                      onPressed: () {
+                        print("jeu 1 pressed");
+                      },
+                      color : Colors.orange,
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(30))),
+                      child: Text('Game 1',style: TextStyle(color:Colors.black),)
+                  ),
+                  RaisedButton(
+                      onPressed: () {
+                        print("Jeu 2 pressed");
+                      },
+                      color : Colors.orange,
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(30))),
+                      child: Text('Game 2',style: TextStyle(color:Colors.black),)
+                  ),
+                ],
+              )
           ),
         ],
       ),
