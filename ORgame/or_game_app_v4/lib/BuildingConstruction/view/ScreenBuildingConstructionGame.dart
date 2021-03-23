94c9cd8cd34705e42f03149d23aa969c1436e3ba
camille24121998 <camille24121998@live.fr>
2021-03-23 12:17:41 +0100

    Ajout cout immeuble

--- ORgame/or_game_app_v4/lib/BuildingConstruction/view/ScreenBuildingConstructionGame.dart
+++ ORgame/or_game_app_v4/lib/BuildingConstruction/view/ScreenBuildingConstructionGame.dart
@@ -33,102 +33,135 @@ class BuildingConstructionGameState extends State<ScreenBuildingConstructionGame
   Widget build(BuildContext context) {
     return Stack(
         children : [
-        Row(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: [
-          Container(
-            width : MediaQuery.of(context).size.width * 0.65,
-            height: MediaQuery.of(context).size.height,
-            //color : Colors.brown,
-            child: ListView(
-                reverse: true,
-              scrollDirection: Axis.vertical,
-                children : [Container(
-                    decoration: BoxDecoration(
-                        image: DecorationImage(
-                          image: AssetImage("assets/images/backgrounds/bgDistributionService.png"),
-                          fit: BoxFit.cover,
-                          alignment: Alignment.bottomCenter,
-                        )
-                    ),
-                    height: 50*BCC.getBCM.getDescriptionOfBuildings.getMaxHeight.toDouble(),
-                    alignment: Alignment.center,
-                    child: ListView.builder(
-                      shrinkWrap: true,
-                        scrollDirection: Axis.horizontal,
-                        itemCount: BCC.getBCM.getDescriptionOfBuildings.getNumberOfBuildings,
-                        itemBuilder: (BuildContext context, int index) {
-                          return Container(
-                            padding: const EdgeInsets.only(left: 20),
-                            child: _buildDragTarget(index+1),
-                          );
-                        },
-                    )
-                )]
-            ),
-          ),
-          Container(
-              width : MediaQuery.of(context).size.width * 0.35,
-              height: MediaQuery.of(context).size.height,
-              color : Colors.white70,
-              child : DragTarget<Client>(
-                builder: (BuildContext context, List<Client> incoming, List rejected) {
-                  return StaggeredGridView.countBuilder(
-                    crossAxisCount: 3,
-                    staggeredTileBuilder: (int index) =>
-                    new StaggeredTile.count(1, BCC.getBCM.getClients[(BCC.getClientsIndexesInBuilding(0))[index]].getRequestOfFloors),
-                    itemCount: BCC.getNumberOfClientsInBuilding(0),
-                    itemBuilder: (BuildContext context, int index) =>
-                        Align(
-                            child: Client_Draggable(BCC.getBCM.getClients[(BCC.getClientsIndexesInBuilding(0))[index]])
-                        ),
-                  );
-                },
-                onWillAccept: (data){
-                  print("OnWillAccept");
-                  return true;
-                },
-                onAccept: (data) {
-                  print("Accept $data");
-                  setState(() {
-                    //successfulDrop = true;
-                    BCC.getBCM.assignClientToBuilding(data.getIndex,0);
-                  });
-                },
-                onLeave: (data) {
-                  print("Not accept");
-                },
-              )
-          )
-              ]
-          ),
+          Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Container(
+                  width : MediaQuery.of(context).size.width * 0.65,
+                  height: MediaQuery.of(context).size.height,
+                  //color : Colors.brown,
+                  child: ListView(
+                      reverse: true,
+                      scrollDirection: Axis.vertical,
+                      children : [
+                        Container(
+                            decoration: BoxDecoration(
+                                image: DecorationImage(
+                                  image: AssetImage("assets/images/backgrounds/bgDistributionService.png"),
+                                  fit: BoxFit.cover,
+                                  alignment: Alignment.bottomCenter,
+                                )
+                            ),
+                            height: 50*BCC.getBCM.getDescriptionOfBuildings.getMaxHeight.toDouble(),
+                            alignment: Alignment.center,
+                            child: Row(
+                                children : [
+                                  Container(
+                                      height: 50*BCC.getBCM.getDescriptionOfBuildings.getMaxHeight.toDouble(),
+                                      width : MediaQuery.of(context).size.width * 0.15,
+                                      child : ListView.builder(
+                                          reverse: true,
+                                          shrinkWrap: true,
+                                          scrollDirection: Axis.vertical,
+                                          itemCount: BCC.getBCM.getDescriptionOfBuildings.getMaxHeight,
+                                          itemBuilder: (BuildContext context, int index){
+                                            return Container(
+                                                height: 50,
+                                                width: 50,
+                                                alignment: Alignment.centerRight,
+                                                child: Text('${BCC.getBCM.getDescriptionOfBuildings.getPricesOfFloors[index].toInt()}', style: TextStyle(
+                                                    fontSize: 18,
+                                                    color: Colors.black,
+                                                    fontWeight: FontWeight.bold,
+                                                    decoration: TextDecoration.none)
+                                                )
+                                            );
+                                          }
+                                      )
+                                  ),
+                                  Container(
+                                      height: 50*BCC.getBCM.getDescriptionOfBuildings.getMaxHeight.toDouble(),
+                                      width : MediaQuery.of(context).size.width * 0.5,
+                                      child : ListView.builder(
+                                        shrinkWrap: true,
+                                        scrollDirection: Axis.horizontal,
+                                        itemCount: BCC.getBCM.getDescriptionOfBuildings.getNumberOfBuildings,
+                                        itemBuilder: (BuildContext context, int index) {
+                                          return Container(
+                                            padding: const EdgeInsets.only(right: 20),
+                                            child: _buildDragTarget(index+1),
+                                          );
+                                        },
+                                      )
+                                  ),
+                                ]
+                            )
+                        )]
+                  ),
+                ),
+                Container(
+                    width : MediaQuery.of(context).size.width * 0.35,
+                    height: MediaQuery.of(context).size.height,
+                    color : Colors.white70,
+                    child : DragTarget<Client>(
+                      builder: (BuildContext context, List<Client> incoming, List rejected) {
+                        return StaggeredGridView.countBuilder(
+                          crossAxisCount: 3,
+                          staggeredTileBuilder: (int index) =>
+                          new StaggeredTile.count(1, BCC.getBCM.getClients[(BCC.getClientsIndexesInBuilding(0))[index]].getRequestOfFloors),
+                          itemCount: BCC.getNumberOfClientsInBuilding(0),
+                          itemBuilder: (BuildContext context, int index) =>
+                              Align(
+                                  child: Client_Draggable(BCC.getBCM.getClients[(BCC.getClientsIndexesInBuilding(0))[index]])
+                              ),
+                        );
+                      },
+                      onWillAccept: (data){
+                        print("OnWillAccept");
+                        return true;
+                      },
+                      onAccept: (data) {
+                        print("Accept $data");
+                        setState(() {
+                          BCC.getBCM.assignClientToBuilding(data.getIndex,0);
+                          BCC.getBCM.updateScore();
+                        });
+                      },
+                      onLeave: (data) {
+                        print("Not accept");
+                      },
+                    ))]),
           Positioned(
               bottom : 0,
-              left : 0,
-              child : Text('Score $score',style : TextStyle(fontSize: 18, color: Colors.black, fontWeight: FontWeight.bold, decoration: TextDecoration.none))
+              right : 0,
+              child : Container (
+                  width : MediaQuery.of(context).size.width * 0.35,
+                  color: Colors.blueGrey,
+                  alignment : Alignment.center,
+                  child : Text('Score ${BCC.getBCM.getScore.toInt()}',style : TextStyle(fontSize: 18, color: Colors.black, fontWeight: FontWeight.bold, decoration: TextDecoration.none))
+              )
           ),
           Positioned(
               top : 0,
               right : 0,
               child : Align(
-                alignment: Alignment.centerRight,
-                child: Padding(
-                    padding: EdgeInsets.all(5.0),
-                    child: ElevatedButton(
-                      child: Text('Return'),
-                      style: Style.returnButtonText,
-                      onPressed: () {
-                        Navigator.of(context).push( //Navigateur vers widget
-                          MaterialPageRoute(builder: (context)=>
-                              ScreenBuildingConstructionLevel(difficulty),
-                          ),
-                        );
-                      },
-                    )
-                ),
+                  alignment: Alignment.centerRight,
+                  child: Padding(
+                      padding: EdgeInsets.all(5.0),
+                      child: ElevatedButton(
+                        child: Text('Return'),
+                        style: Style.returnButtonText,
+                        onPressed: () {
+                          Navigator.of(context).push( //Navigateur vers widget
+                            MaterialPageRoute(builder: (context)=>
+                                ScreenBuildingConstructionLevel(difficulty),
+                            ),
+                          );
+                        },
+                      )
+                  )
               )
-          ),
-        ]
+          )]
     );
   }
 
@@ -146,7 +179,7 @@ class BuildingConstructionGameState extends State<ScreenBuildingConstructionGame
             height: 50*BCC.getBCM.getDescriptionOfBuildings.getMaxHeight.toDouble(),
             width: 50,
             child : ListView.builder(
-              reverse: true,
+                reverse: true,
                 physics: NeverScrollableScrollPhysics(),
                 itemCount: BCC.getClientsIndexesInBuilding(index_building).length,
                 itemBuilder: (BuildContext context, int index) {
@@ -163,6 +196,7 @@ class BuildingConstructionGameState extends State<ScreenBuildingConstructionGame
         print("Accept $data");
         setState(() {
           BCC.getBCM.assignClientToBuilding(data.getIndex,index_building);
+          BCC.getBCM.updateScore();
         });
       },
       onLeave: (data) {
