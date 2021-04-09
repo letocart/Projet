86bf5e389b1dfec3d4d6efd3b9388c99f552438d
fatibellafkih <fatima.zahra.blfk@gmail.com>
2021-04-09 16:48:35 +0200

    level / orgame UI / storage Util

--- ORgame/or_game_app_v4/lib/ORgameUI.dart
+++ ORgame/or_game_app_v4/lib/ORgameUI.dart
@@ -6,15 +6,18 @@ import 'view/ScreenHome.dart';
 import 'dart:async' show Future;
 import 'package:flutter/services.dart' show rootBundle;
 
-// creation de la classe widget ROgameUI est un StatefulWidget (widget pouvant changer d'etat)
+// The ORgameUI Class which helps to define the screen orientation through the whole application
 class ORgameUI extends StatefulWidget {
   final ORgameUIState state = ORgameUIState();
   State<StatefulWidget> createState() => state;
 }
 
+//the ORgameUI State
 class ORgameUIState extends State<ORgameUI> with WidgetsBindingObserver {
 
+  // Building UI
   Widget build(BuildContext context) {
+    // Setting the device orientation
     SystemChrome.setPreferredOrientations([
       DeviceOrientation.landscapeLeft,
       DeviceOrientation.landscapeRight,
